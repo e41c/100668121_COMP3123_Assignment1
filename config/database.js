28793718fff6d83e966f8f6075e149c3df357a38
e41c <ericgrigor@gmail.com>
2023-10-15 13:01:47 -0400

    working on errors, trying to connect to MongoDB Atlas

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-// Replace '3FiLVFHmjtLpYszS' with your actual MongoDB Atlas password
 const password = '3FiLVFHmjtLpYszS';
 
-// Define the MongoDB Atlas connection URL
 const mongoURL = `mongodb+srv://e41c:${password}@cluster0.ijkhohf.mongodb.net/comp3123_assignment1`;
 
 mongoose.connect(mongoURL, {
@@ -11,7 +9,6 @@ mongoose.connect(mongoURL, {
   useUnifiedTopology: true,
 });
 
-// Handle MongoDB connection events
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
 });
