9cb8f38958f04bf6fa4f46e7207a2fbaa1295548
e41c <ericgrigor@gmail.com>
2023-10-15 12:15:14 -0400

    second commit, built basic structure

---
+++ routes/employee.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const Employee = require('../models/Employee');
+
+// Define your routes for employee-related operations
+// Example: GET /api/v1/emp/employees
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Define your routes for user-related operations
+// Example: POST /api/v1/user/signup
+
+module.exports = router;
