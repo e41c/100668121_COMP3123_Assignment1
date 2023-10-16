2016d577cb9ea391b3d6662a3217a262419fbb00
e41c <ericgrigor@gmail.com>
2023-10-15 22:34:36 -0400

    moved database info from database.js, directly into app.js

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const database = require('./config/database');
+// const database = require('./config/database');
 
-const DB_CONNECTION_STRING = "mongodb+srv://e41c:3FiLVFHmjtLpYszS@cluster0.ijkhohf.mongodb.net/?retryWrites=true&w=majority"
+// const DB_CONNECTION_STRING = "mongodb+srv://e41c:3FiLVFHmjtLpYszS@cluster0.ijkhohf.mongodb.net/?retryWrites=true&w=majority"
+
+// Testing database.js code, directly into app.js
+const mongoose = require('mongoose');
+
+const password = '3FiLVFHmjtLpYszS';
+
+const mongoURL = `mongodb+srv://e41c:${password}@cluster0.ijkhohf.mongodb.net/comp3123_assignment1`;
+
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error: ' + err);
+});
+
+module.exports = mongoose;
+
 
 
 // Middleware
 app.use(bodyParser.json());
+app.use(express.json());
+
 
 // Routes
 const userRoutes = require('./routes/user');
--- config/database.js
+++ config/database.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const password = '3FiLVFHmjtLpYszS';
+// const password = '3FiLVFHmjtLpYszS';
 
-const mongoURL = `mongodb+srv://e41c:${password}@cluster0.ijkhohf.mongodb.net/comp3123_assignment1`;
+// const mongoURL = `mongodb+srv://e41c:${password}@cluster0.ijkhohf.mongodb.net/comp3123_assignment1`;
 
-mongoose.connect(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(mongoURL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
+// mongoose.connection.on('connected', () => {
+//   console.log('Connected to MongoDB');
+// });
 
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error: ' + err);
-});
+// mongoose.connection.on('error', (err) => {
+//   console.error('MongoDB connection error: ' + err);
+// });
 
-module.exports = mongoose;
+// module.exports = mongoose;
