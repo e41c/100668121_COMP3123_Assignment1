9979df6cc376723ee830dfd5035fb7288ce4f073
e41c <ericgrigor@gmail.com>
2023-10-15 23:19:48 -0400

    fixed mongodb access, able to connect

--- controllers/userController.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/userModel');
-
-exports.signup = async (req, res) => {
-  // Implement user signup logic
-};
-
-exports.login = async (req, res) => {
-  // Implement user login logic
-};
